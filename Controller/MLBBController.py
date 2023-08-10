94461e062b24f15762bea79cf1998092678564d5
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-08-10 15:02:27 +0000

    function for load data username and winrate

---
+++ Controller/MLBBController.py
@@ -0,0 +1,61 @@
+import requests, json
+
+
+class MLBBController:
+
+  @staticmethod
+  def check_username_help():
+    with open('json/check_username.json', 'r') as json_file:
+      data = json.load(json_file)
+    return data
+
+  @staticmethod
+  def check_username(user_id, zone_id):
+    url = "https://mainlagiaja.com/ajax/games/check/mobile-legend"
+    payload = {"user_id": user_id, "zone_id": zone_id}
+
+    response = requests.post(
+      url,
+      data=payload,
+      headers={"Content-Type": "application/x-www-form-urlencoded"})
+    result = response.json()
+
+    if result.get('ok'):
+      formatted_response = {
+        "msg": "data player ditemukan",
+        "name": result.get('name'),
+        "ok": True,
+        "user_id": user_id,
+        "zone_id": zone_id
+      }
+    else:
+      formatted_response = {
+        "msg": "Invalid User ID Or Zone ID",
+        "ok": False,
+        "user_id": user_id,
+        "zone_id": zone_id
+      }
+
+    return formatted_response
+
+  @staticmethod
+  def calculate_winrate_response(tMatch, tWr, wrReq):
+    result_num = MLBBController.calculate_winrate(tMatch, tWr, wrReq)
+    response_text = f"Kamu memerlukan sekitar {result_num} win tanpa lose untuk mendapatkan win rate {wrReq}%"
+    response = {
+      "2_total_match": tMatch,
+      "3_total_wr": tWr,
+      "4_winrate_target": wrReq,
+      "5_response": response_text
+    }
+    return response
+
+  @staticmethod
+  def calculate_winrate(tMatch, tWr, wrReq):
+    tWin = tMatch * (tWr / 100)
+    tLose = tMatch - tWin
+    sisaWr = 100 - wrReq
+    wrResult = 100 / sisaWr
+    seratusPersen = tLose * wrResult
+    final = seratusPersen - tMatch
+    return round(final)
