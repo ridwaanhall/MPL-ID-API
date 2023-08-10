dd9042488463f9e1629c5a81b883a8a6e24eedb3
ridwaanhall <91854659+ridwaanhall@users.noreply.github.com>
2023-08-10 15:50:34 +0000

    Add route for help

--- server.py
+++ server.py
@@ -1,18 +1,25 @@
 from flask import Flask, redirect, url_for
 from Controller.MLBBController import MLBBController
+from Controller.HelpController import HelpController
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello_world():
-  return redirect(url_for('check_username_help'))
+  return redirect(url_for('global_help'))
+
+
+# dashboard to Help
+@app.route('/help', methods=['GET'])
+def global_help():
+  return HelpController.global_help()
 
 
 # dashboard to check username
 @app.route('/check-username', methods=['GET'])
 def check_username_help():
-  return MLBBController.check_username_help()
+  return HelpController.check_username_help()
 
 
 # to get username by userid and zoneid
@@ -24,7 +31,7 @@ def check_username(user_id, zone_id):
 # dashboard to count winrat target
 @app.route('/winrate-target', methods=['GET'])
 def calculate_winrate_help():
-  return MLBBController.calculate_winrate_help()
+  return HelpController.calculate_winrate_help()
 
 
 # to target winrate
